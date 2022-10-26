f3b695daed9c1cdfed4c92bb148296cc061c98ba
NwaguChima <nwaguchima48@gmail.com>
2022-10-26 16:09:30 +0100

    setup carousel

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="relative">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
