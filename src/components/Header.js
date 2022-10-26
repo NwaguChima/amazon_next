5b079a39f47e037e84df71af3a2b000e4f06bf1e
NwaguChima <nwaguchima48@gmail.com>
2022-10-26 16:01:22 +0100

    added nav items

--- src/components/Header.js
+++ src/components/Header.js
@@ -52,11 +52,20 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div>
-        <p>
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white">
+        <p className="link flex items-center">
           <MenuIcon className="h-6 mr-2" />
           All
         </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
       </div>
     </header>
   );
