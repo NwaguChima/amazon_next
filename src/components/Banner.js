233762c57ee75214b3dae659f1a06f92845475f3
NwaguChima <nwaguchima48@gmail.com>
2022-10-26 16:11:06 +0100

    added images

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,9 +12,17 @@ const Banner = () => {
         showThumbs={false}
         interval={5000}
       >
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/gi1" alt="" />
+        </div>
+
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
+        </div>
+
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/7ma" alt="" />
+        </div>
       </Carousel>
     </div>
   );
