cd4fe511dc4293cd18da78f9a1b4e2a3c9576745
NwaguChima <nwaguchima48@gmail.com>
2022-10-26 02:34:52 +0100

    completed left side header

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,8 +20,13 @@ const Header = () => {
           />
         </div>
 
-        <div className="bg-yellow-400">
-          <input type="text" name="search" id="search" />
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+          />
           <SearchIcon className="h-12 p-4" />
         </div>
       </div>
