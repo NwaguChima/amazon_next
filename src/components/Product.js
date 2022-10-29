fbd578574e41ab1de218fe7251c9d749be573607
nwagu chima <chima.nwagu@decagon.dev>
2022-10-29 11:14:07 +0100

    rendered all products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
+import Currency from "react-currency-formatter";
+
+const MAX_RATING = 5;
+const MIN_RATING = 1;
 
 const Product = ({ id, title, price, description, category, image }) => {
-  const [rating] = useState(Math.floor(Math.random() * (5 - 1 + 1)) + 1);
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+
+  const [hasPrime] = useState(Math.random() < 0.5);
 
   return (
     <div>
@@ -20,6 +28,21 @@ const Product = ({ id, title, price, description, category, image }) => {
             <StarIcon className="h-5" />
           ))}
       </div>
+
+      <p>{description}</p>
+
+      <div>
+        <Currency quantity={price} currency="NGN" />
+      </div>
+
+      {hasPrime && (
+        <div>
+          <img src="https://links.papareact.com/fdw" alt="" />
+          <p>FREE Next-day Delivery</p>
+        </div>
+      )}
+
+      <button>Add to Basket</button>
     </div>
   );
 };
