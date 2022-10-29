379e0ac5cd07b36694531a6abf729e84095619a2
nwagu chima <chima.nwagu@decagon.dev>
2022-10-29 11:08:24 +0100

    rendered card items

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,27 @@
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
+import { StarIcon } from "@heroicons/react/solid";
 
 const Product = ({ id, title, price, description, category, image }) => {
-  return <div>Product</div>;
+  const [rating] = useState(Math.floor(Math.random() * (5 - 1 + 1)) + 1);
+
+  return (
+    <div>
+      <p>{category}</p>
+
+      <Image src={image} width={200} height={200} objectFit="contain" />
+
+      <h4>{title}</h4>
+
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-5" />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Product;
