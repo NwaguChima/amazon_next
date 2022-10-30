baae372b1192d4bee6bd76becfb2f8a5a21a7dd0
NwaguChima <nwaguchima48@gmail.com>
2022-10-30 12:42:25 +0100

    setup webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,10 +1,34 @@
 import { buffer } from "micro";
 import * as admin from "firebase-admin";
 
+// Secure a connection to Firebase from the backend.
 let serviceAccount = require("../../../permissions.json");
+
+const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+  : admin.app();
+
+// Establish a connection to Stripe.
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
 export default async (req, res) => {
   if (req.method === "POST") {
     // Process a POST request
+    const requestBuffer = await buffer(req);
+    const payload = requestBuffer.toString();
+    const sig = req.headers["stripe-signature"];
+
+    let event;
+
+    // Verify that the event posted came from Stripe.
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (err) {
+      console.log(`❌ Error message: ${err.message}`);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
   }
 };
