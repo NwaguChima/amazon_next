8141de189fbab5ea5aa40412d2e6dc6b8027015b
NwaguChima <nwaguchima48@gmail.com>
2022-10-30 12:51:11 +0100

    completed fulfiled order

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -14,6 +14,26 @@ const app = !admin.apps.length
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
+const fulfillOrder = async (session) => {
+  console.log("Fulfilling order", session);
+
+  return app
+    .firestore()
+    .collection("users")
+    .doc(session.metadata.email)
+    .collection("orders")
+    .doc(session.id)
+    .set({
+      amount: session.amount_total / 100,
+      amount_shipping: session.total_details.amount_shipping / 100,
+      images: JSON.parse(session.metadata.images),
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+      console.log(`SUCCESS: Order ${session.id} has been added to the DB!`);
+    });
+};
+
 export default async (req, res) => {
   if (req.method === "POST") {
     // Process a POST request
@@ -29,6 +49,16 @@ export default async (req, res) => {
     } catch (err) {
       console.log(`❌ Error message: ${err.message}`);
       return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the checkout.session.completed event.
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      // Fulfill the purchase...
+      return fulfillOrder(session)
+        .then(() => res.status(200))
+        .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
     }
   }
 };
