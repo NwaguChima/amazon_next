cb634346bc49825fb5a85a896dc515c772129752
NwaguChima <nwaguchima48@gmail.com>
2022-10-30 12:35:59 +0100

    added permission file

---
+++ src/pages/api/webhook.js
@@ -0,0 +1,10 @@
+import { buffer } from "micro";
+import * as admin from "firebase-admin";
+
+let serviceAccount = require("../../../permissions.json");
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    // Process a POST request
+  }
+};
